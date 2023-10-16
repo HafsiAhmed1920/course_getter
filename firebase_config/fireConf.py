dbf64e2b85299abccf6f435fdf187761e863e727
HafsiAhmed1920 <126480771+HafsiAhmed1920@users.noreply.github.com>
2023-10-16 17:08:06 +0200

    Update fireConf.py

--- firebase_config/fireConf.py
+++ firebase_config/fireConf.py
@@ -5,4 +5,4 @@ from firebase_admin import credentials
 def initialise_firebase():
     cred = credentials.Certificate(r'Path to your credentiels\data-pipeline.json')
     firebase_admin.initialize_app(cred,
-                                  {'storageBucket': your-bucket-url.com'})
+                                  {'storageBucket': 'your-bucket-url.com'})
