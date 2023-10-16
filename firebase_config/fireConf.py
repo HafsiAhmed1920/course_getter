27a105d43b1f8bad01102eeaf687ebf1c6a73259
HafsiAhmed1920 <126480771+HafsiAhmed1920@users.noreply.github.com>
2023-10-16 17:07:21 +0200

    Update fireConf.py

--- firebase_config/fireConf.py
+++ firebase_config/fireConf.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials
 
 
 def initialise_firebase():
-    cred = credentials.Certificate(r'C:\Users\ahafsi\project beta\data-pipeline.json')
+    cred = credentials.Certificate(r'Path to your credentiels\data-pipeline.json')
     firebase_admin.initialize_app(cred,
                                   {'storageBucket': 'datapipe-aad93.appspot.com'})
